709be8b4b5cf0964f10f34846bc40599c9c121f3
romariou76 <illeniumr@gmail.com>
2022-09-12 09:14:19 -0500

    Diseñando la pagina de inicio

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,9 +4,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
     <title>Document</title>
 </head>
 <body>
-    <h1>Bienvenido perro</h1>
+<div class="navbar">
+    <div class="title">
+        Romario APP
+    </div>
+    <div class="navbar-items">
+        <a href="#home">Home</a>
+        <a href="#news">News</a>
+        <a href="#about">About</a>
+        <a href="#about">Tutoriales</a>
+        <a href="#about">Iniciar Sesion</a>
+        <a href="#about">Salir</a>
+    </div>
+</div>
+<div>
+
+</div>
+            
+
+<div class='foter'>
+    <p>Copyright © 2022 Mejorando dia a dia / Desarrollado por Romario</p>
+</div>
+
+
 </body>
 </html>
